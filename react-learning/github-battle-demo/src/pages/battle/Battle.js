8387ece42c9998d7d56438d706100961bc87c45c
kwm42 <kwm2596123568@gmail.com>
2019-05-23 21:24:20 +0800

    add battle component

--- react-learning/github-battle-demo/src/pages/battle/Battle.js
+++ react-learning/github-battle-demo/src/pages/battle/Battle.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import './Battle.css'
 
 class Battle extends React.Component{
+
+    constructor(){
+        super()
+        this.state={
+            playerOne: null,
+            playerTwo: null
+        }
+    }
+
     render(){
         return (
-            <div>
-                <h1>Battle</h1>
+            <div className="battle-wrapper">
+                <div className="player">
+                    <div>
+                        <input type="text" placeholder="player one name here"/>
+                        <button>confirm</button>
+                        { this.state.playerOne ? (<img alt="" src={ this.state.playerOne && this.state.playerOne.avatar_url }/>) : <span />}
+                    </div>
+                </div>
+                <div className="player">
+                    <div>
+                        <input type="text" placeholder="player one name here"/>
+                        <button>confirm</button>
+                        { this.state.playerOne ? (<img alt="" src={ this.state.playerOne && this.state.playerOne.avatar_url }/>) : <span />}
+                    </div>
+                </div>
             </div>
         )
     }
