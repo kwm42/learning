f9dd097ebdd8d5bff5d9ee33a2f71fb675f3f7c0
kwm42 <kwm2596123568@gmail.com>
2019-05-24 08:54:59 +0800

    modify Battle component and add styles

--- react-learning/github-battle-demo/src/pages/battle/Battle.js
+++ react-learning/github-battle-demo/src/pages/battle/Battle.js
@@ -16,20 +16,38 @@ class Battle extends React.Component{
             <div className="battle-wrapper">
                 <div className="player">
                     <div>
-                        <input type="text" placeholder="player one name here"/>
-                        <button>confirm</button>
+                        <input type="text" placeholder="player one name here" ref="input1" />
+                        <button onClick={ () => this.getUserInfo(1) }>confirm</button>
                         { this.state.playerOne ? (<img alt="" src={ this.state.playerOne && this.state.playerOne.avatar_url }/>) : <span />}
                     </div>
                 </div>
                 <div className="player">
                     <div>
-                        <input type="text" placeholder="player one name here"/>
-                        <button>confirm</button>
-                        { this.state.playerOne ? (<img alt="" src={ this.state.playerOne && this.state.playerOne.avatar_url }/>) : <span />}
+                        <input type="text" placeholder="player two name here" ref="input2" />
+                        <button onClick={ () => this.getUserInfo(2) }>confirm</button>
+                        { this.state.playerTwo ? (<img alt="" src={ this.state.playerTwo && this.state.playerTwo.avatar_url }/>) : <span />}
                     </div>
                 </div>
             </div>
         )
+    }
+
+    getUserInfo(index){
+        let username
+        username = (index === 1)?this.refs.input1.value : this.refs.input2.value
+        fetch('https://api.github.com/users/'+username)
+            .then(res => res.json())
+            .then(data => {
+                if(index === 1){
+                    this.setState({
+                        playerOne: data
+                    })
+                }else{
+                    this.setState({
+                        playerTwo: data
+                    })
+                }
+            })
     }
 }
 
