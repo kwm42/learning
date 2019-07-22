344179f16d89d65767c0f7b8abb2e2be8cc384a5
kwm42 <kwm2596123568@gmail.com>
2019-07-22 21:40:53 +0800

    rm nodejs soider

--- nodejs-spider/service/testService.js
+++
@@ -1,9 +0,0 @@
-const request = require('superagent');
-
-
-exports.testRequest = function() {
-  request.get('http://www.baidu.com')
-    .end(function(err, res) {
-      console.log(err, res);
-    })
-}
